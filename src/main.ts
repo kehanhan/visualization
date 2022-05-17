43737c83d33be069e74ff8be05b0ab5306dff7b2
kehanhan <ke_hanhan@outlook.com>
2022-05-17 12:29:33 +0800

    大屏适配

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./assets/styles/global.scss";
 createApp(App).mount("#app");
+function setSize() {
+  const SCALE = 16 / 9;
+  const cWidth = document.documentElement.clientWidth;
+  const cHeight = document.documentElement.clientHeight;
+  const width = cWidth / cHeight > SCALE ? cHeight * SCALE : cWidth;
+  const height = width / SCALE;
+  const app = (window as any).app;
+  app.style.width = width + "px";
+  app.style.height = height + "px";
+  app.style.marginTop = (cHeight - height) / 2 + "px";
+  document.documentElement.style.fontSize = `${width / 100}px`;
+}
+setSize();
+window.onresize = setSize;
